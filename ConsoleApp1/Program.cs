e05745d913679e77fad0ff4d539774cbae9f4dee
Your Name <you@example.com>
2018-01-22 16:46:56 -0300

    primer commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Dime el primer numero entero: ");
+            //string leerlineauno = Console.ReadLine();
+            //int convertirenterouno = int.Parse(leerlineauno);
+            //int resultado;
+
+            //for (int i = 1; i <= convertirenterouno; i++)
+            //{
+            //    if (i <= convertirenterouno)
+            //    {
+            //        resultado = convertirenterouno % i;
+            //        if (resultado == 0)
+            //        {
+            //            Console.Write(" " + i + " ");
+            //        }
+            //        if (convertirenterouno < 0)
+            //        {
+            //            Console.WriteLine("¡Le he pedido un número entero mayor que cero!");
+            //        }
+            //    }
+            //}
+
+            //divisores exactos de 9 = imprimir banana
+            //divisores exactos de 5 = imprimir pija  
+            //divisores exactos de 4 = imprimir cornaleon
+            //para el resto = imprimir default
+
+            //Console.Write("Dime el primer numero entero: ");
+            //int numeroIngresado = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= numeroIngresado; i++)
+            //{
+            //    if (i % 9 == 0)
+            //    {
+            //        Console.WriteLine("banana");
+            //    }
+            //    else if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine("pija");
+            //    }
+            //    else if (i % 4 == 0)
+            //    {
+            //        Console.WriteLine("cornaleon");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("default");
+            //    }
+            //}
+
+            //Mostrar los numeros impares entre el 0 y el 100
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.Write("{0}|", i); 
+            //    }
+            //}
+
+            //Mostrar los números del 100 al 0
+            //for (int i = 100; i > -1; i--)
+            //    Console.Write("{0}|", i);
+
+            Console.ReadKey();
+        }
+    }
+}
